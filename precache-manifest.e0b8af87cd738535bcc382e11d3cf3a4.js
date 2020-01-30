954c057869580c593f9080b68f1138d00625ff90
raymag <prayk11@outlook.com>
2020-01-30 10:23:47 -0300

    init

---
+++ precache-manifest.e0b8af87cd738535bcc382e11d3cf3a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68345b756d93d101271c38459df1f3c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1732a4ae1fbac055ae",
+    "url": "/static/css/main.862c731e.chunk.css"
+  },
+  {
+    "revision": "aaa324a60616bf870a76",
+    "url": "/static/js/2.66c8d706.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.66c8d706.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd1732a4ae1fbac055ae",
+    "url": "/static/js/main.446edb75.chunk.js"
+  },
+  {
+    "revision": "79ea1ff585345e4fcdf2",
+    "url": "/static/js/runtime-main.3a2b6185.js"
+  }
+]);
